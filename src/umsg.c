ab44f25296cfa06105d3e68e7c1e460052d977f6
Alex Pabouctsidis <alex.pabouctsidis@gmail.com>
2022-09-06 18:43:05 +0200

    wip

--- src/umsg.c
+++ src/umsg.c
@@ -2,5 +2,27 @@
 // Created by A.Pabouctsidis on 9/6/2022.
 //
 
+#include <FreeRTOS.h>
+#include <Queue.h>
+#include <stdint.h>
+
 #include "../msgs/sensors.h"
 #include "../msgs/control.h"
+
+
+typedef struct
+{
+    QueueHandle_t queue;
+    uint32_t prescaler
+} umsg_sub_t;
+
+typedef struct
+{
+    umsg_sub_t sub;
+    umsg_sub_t* next_sub;
+} umsg_sub_linked_list_t;
+
+typedef struct
+{
+    umsg_sub_linked_list_t* subs;
+} umsg_t;
