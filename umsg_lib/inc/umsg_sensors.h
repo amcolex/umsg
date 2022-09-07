4fbdaeb7cde4c82c543d0c7a3631799902690cb8
Alex Pabouctsidis <alex.pabouctsidis@flyability.com>
2022-09-07 23:52:45 +0200

    reverted sensor mistake

--- umsg_lib/inc/umsg_sensors.h
+++ umsg_lib/inc/umsg_sensors.h
@@ -4,24 +4,28 @@
 #include <umsg_types.h>
 
 // msg structure typedefs
-
-typedef enum
+typedef struct
 {
-    BATTERY_OK,
-    BATTERY_DEAD,
-    BATTERY_COLD
-} battery_state_t;
+    float gyro[3];
+    float accel[3];
+    float temperature;
+} umsg_sensors_imu_t;
 
 typedef struct
 {
-    battery_state_t state;
-    uint8_t low_battery : 1, critical_battery : 1, high_temperature : 1, low_temperature : 1, charging : 1, discharging : 1, full : 1, not_present : 1;
-} umsg_sensors_battery_t;
+    float pressure;
+    float temperature;
+} umsg_sensors_baro_t;
 
 
 // api function headers
-umsg_queue_handle_t umsg_sensors_battery_subscribe(uint32_t prescaler, uint8_t length);
-void umsg_sensors_battery_publish(umsg_sensors_battery_t* data);
-uint8_t umsg_sensors_battery_receive(umsg_queue_handle_t queue, umsg_sensors_battery_t* data, uint32_t timeout);
-uint8_t umsg_sensors_battery_peek(umsg_sensors_battery_t* data);
+umsg_queue_handle_t umsg_sensors_imu_subscribe(uint32_t prescaler, uint8_t length);
+void umsg_sensors_imu_publish(umsg_sensors_imu_t* data);
+uint8_t umsg_sensors_imu_receive(umsg_queue_handle_t queue, umsg_sensors_imu_t* data, uint32_t timeout);
+uint8_t umsg_sensors_imu_peek(umsg_sensors_imu_t* data);
+
+umsg_queue_handle_t umsg_sensors_baro_subscribe(uint32_t prescaler, uint8_t length);
+void umsg_sensors_baro_publish(umsg_sensors_baro_t* data);
+uint8_t umsg_sensors_baro_receive(umsg_queue_handle_t queue, umsg_sensors_baro_t* data, uint32_t timeout);
+uint8_t umsg_sensors_baro_peek(umsg_sensors_baro_t* data);
 
